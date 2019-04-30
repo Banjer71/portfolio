631f10dac4ae18ee99fae3d887f1a4847e709c41
Banjer71 <davgio7@gmail.com>
2019-04-30 11:33:24 +0100

    Updates

---
+++ precache-manifest.b008c940b9204fb7d0c0c8b19402b5c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66cfd23624b1002b07c27c23c6e9a77",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "43e089cecf5f88e9cf32",
+    "url": "/portfolio/static/css/main.53ad468b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "43e089cecf5f88e9cf32",
+    "url": "/portfolio/static/js/main.6774e585.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
